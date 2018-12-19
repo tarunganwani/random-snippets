1c622c9cf5bdab4c0002d9fc586b5a3a0d58a2e0
tarunganwani <tarun.ganwani@gmail.com>
2018-12-19 16:18:24 +0530

    Smart pointer with ref counting

---
+++ basic_smartptr.cpp
@@ -0,0 +1,145 @@
+#include<iostream>
+
+using namespace std;
+
+class Data{
+	int d;
+	public:
+	Data():d(0){}
+	Data(int _d) : d(_d){}
+	void Incr(){
+		d++;
+		cout << "INCR d = " << d << endl;
+	}
+	void operator *(){
+		Display();
+	}
+	void Display(){ 
+		cout << "DISP d = " << d << endl;
+	}
+};
+
+
+class Reference{
+	size_t count;
+	public:
+	Reference():count(0){}
+	size_t IncrRefCount(){
+		return ++count;
+	}
+	size_t DecrRefCount(){
+		return --count;
+	}
+	void DisplayRefCount(){
+		cout << "\tREF COUNT "<< count << endl;
+	}
+};
+
+
+template<typename T>
+class SmartPointer{
+	T *pt;
+	Reference *r; //reference count
+	public:
+
+	SmartPointer() : pt(NULL), r(NULL) {}
+
+	SmartPointer(T *_pt) : pt(_pt), r(NULL){
+		r = new Reference();
+		r->IncrRefCount();
+		cout << "!!! CONSTRUCTOR !!!";
+		r->DisplayRefCount();
+	}
+
+	~SmartPointer(){ 
+		size_t rc = r->DecrRefCount();
+		cout << "DESTRUCTOR";
+		r->DisplayRefCount();
+		if(rc == 0){
+			cout << "\t*** DELETING MEMORY ***" << endl;
+			delete pt; 
+			delete r;
+		}
+	}
+
+	SmartPointer(SmartPointer &rhs){
+		pt = &(*rhs);
+		r = rhs.GetReference();
+		r->IncrRefCount();
+		cout << "COPY CONSTRUCTOR";
+		r->DisplayRefCount();
+	}	
+	
+	T &operator *(){
+		return *pt;
+	}
+
+	T *operator ->(){
+		return pt;
+	}
+
+	Reference *GetReference(){
+		return r;
+	}
+
+	SmartPointer<T> &operator=(SmartPointer<T> &rhs){
+		
+		if(this == &rhs){
+			return *this;
+		}
+		
+		pt = &(*rhs);
+		r = rhs.GetReference();
+		r->IncrRefCount();
+		
+		cout << "ASSIGNMENT OPERATOR";
+		r->DisplayRefCount();
+
+		
+
+		
+	}	
+
+	/*SmartPointer<T> &operator=(const SmartPointer<T> &rhs){
+		if(this == &rhs){
+		       return *this;
+		}
+		pt = &(*rhs);
+		rc = GetRC();
+		rc++;
+		cout << "ASSIGNMENT RC " << rc << endl;
+
+	}*/
+
+};
+
+template<typename T>
+void foo(SmartPointer<T> sp){
+	cout << endl << "FOO" << endl;
+	sp->Incr();
+	(*sp).Display();
+	cout << "FOO END" << endl << endl;
+}
+
+int main(){
+
+	SmartPointer<Data> sp(new Data());
+	sp->Incr();
+	(*sp).Display();
+
+	{
+		SmartPointer<Data> sp2(sp);
+		sp->Incr();
+		(*sp2).Display();
+	}
+
+	cout << "CALLING FOO" << endl;
+	foo(sp);
+
+	SmartPointer<Data> sp3;
+	sp3 = sp;
+
+	(*sp).Incr();
+	sp->Display();
+	return 0;
+}
